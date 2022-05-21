83fea9752186a9ee95ca7cf9e72cbe54cdb38aa1
martino-lucas95 <71530057+martino-lucas95@users.noreply.github.com>
2022-05-21 14:35:58 -0300

    commit 1

--- src/Library/Barcos/Acorazado.cs
+++ src/Library/Barcos/Acorazado.cs
@@ -2,11 +2,11 @@ using System;
 using System.Collections;
 namespace BatallaNaval
 {
-    public class Acorasado : Barcos_Abstract
+    public class Acorazado : Barcos_Abstract
     {
-        public static string nombre = "Acorasado";
+        public static string nombre = "Acorazado";
 
-        public Acorasado(int p1x, int p1y, int p2x, int p2y) :base(nombre, 3, p1x, p1y, p2x, p2y)
+        public Acorazado(int p1x, int p1y, int p2x, int p2y) :base(nombre, 3, p1x, p1y, p2x, p2y)
         {    
         }
     }
--- src/Library/Logica.cs
+++ src/Library/Logica.cs
@@ -10,7 +10,7 @@ namespace BatallaNaval
         {
 
         }
-        public void BarcoUndido()
+        public void BarcoHundido()
         {
 
         }

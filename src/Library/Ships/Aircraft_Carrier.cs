d30bdabfe473dfb558cb1f741454ef7d5a7c0003
sofi <sofihernande123@gmail.com>
2022-06-09 19:55:13 -0300

    cambio

--- src/Library/Ships/Aircraft_Carrier.cs
+++ src/Library/Ships/Aircraft_Carrier.cs
@@ -5,6 +5,7 @@ namespace BatallaNaval
     public class Aircraft_Carrier : Ships
     {
         public static string name = "Aircraft carrier";
+        public static int size=4;
 
         public Aircraft_Carrier(int x, int y, int size, Orientation o) :base( x, y, size, o)
         {    
--- src/Library/Ships/Cruiser.cs
+++ src/Library/Ships/Cruiser.cs
@@ -5,6 +5,7 @@ namespace BatallaNaval
     public class Cruiser : Ships
     {
         public static string name = "Cruiser";
+        public static int size=3;
 
         public Cruiser(int x, int y, int size, Orientation o) :base( x, y, size, o)
         {    

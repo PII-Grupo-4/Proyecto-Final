c0fbe2eda2fd9fb6d668c4698046e0443514fdc5
sofi <sofihernande123@gmail.com>
2022-06-21 20:29:54 -0300

    sofi

---
+++ src/Library/Handlers3/SeeBoardsHandle.cs
@@ -0,0 +1,49 @@
+
+namespace Battleship
+{
+    /// <summary>
+    /// Un "handler" del patrón Chain of Responsibility que implementa el comando "ver tableros".
+    /// </summary>
+    public class SeeBoardsHandle : BaseHandler
+    {
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase <see cref="SeeBoardsHandle"/>. Esta clase procesa el mensaje "ver tableros".
+        /// </summary>
+        /// <param name="next">El próximo "handler".</param>
+        public SeeBoardsHandle(BaseHandler next) : base(next)
+        {
+            this.Keywords = new string[] {"ver tableros", "tableros", "tablero", "ver tablero"};
+        }
+
+        /// <summary>
+        /// Procesa el mensaje "ver tableros" y retorna true; retorna false en caso contrario.
+        /// </summary>
+        /// <param name="message">El mensaje a procesar.</param>
+        /// <param name="response">La respuesta al mensaje procesado.</param>
+        /// <returns>true si el mensaje fue procesado; false en caso contrario.</returns>
+        protected override void InternalHandle(Message message, out string response)
+        {
+            try
+            {
+                User user = UserRegister.GetUser(message.id);
+
+                if (user.getStatus() != "position ships" && user.getStatus() != $"in {user.GetGameMode()} game")
+                {
+                    // Estado de user incorrecto
+                    response = $"Comando incorrecto. Estado del usuario = {user.getStatus()}";
+                    return;
+                }
+                else
+                {
+                    // Devolviendo los tableros como string
+                    response = user.GetPlayer().GetBoardsToPrint();
+                }
+                
+            }
+            catch
+            {
+                response = "Sucedió un error";
+            }
+        }
+    }
+}

6ab3d868c8cb2742165eac1e1764753a50c5f088
Iván_Casales <casalesivan@hotmail.com>
2022-06-21 09:03:30 -0300

    more tests

--- src/Test/03 - PositionShips/PositionShipsTests.cs
+++ src/Test/03 - PositionShips/PositionShipsTests.cs
@@ -3,22 +3,22 @@ using Battleship;
 
 namespace Library.Tests
 {
-    // Existe un handler encargado de posicionar los barcos, sin embargo dicho handler contiene
-    // inputs y prints de información dentro del internal handler, por lo que no podemos
-    // testearlo directamente como un handler común. La idea en un inicio fue que se ingresara
-    // la palabra clave "posicionar barco" acompañada de la coordenada, pero por el momento 
-    // no hemos podido realizarlo ya que el ingreso de las distintas coordenadas no evi
-    public class PositionShipsTest
+    // El siguiente Handler es el encargado de posicionar los barcos, se le indica la 
+    // coordenada y la orientación del mismo. Por convensión se coloca del mas grande
+    // al mas pequeño. En total hay 4 barcos que varian el tamaño desde 2 a 5 lugares c/u
+    public class PositionShipsHandleTest
     {
-        SeeBoardsHandle handler;
+        PositionShipsHandle handler;
         Message message;
         User user1;
         User user2;
 
+        Game game;
+
         [SetUp]
         public void Setup()
         {
-            handler = new SeeBoardsHandle(null);
+            handler = new PositionShipsHandle(null);
             message = new Message();
 
             user1 = new User("a");
@@ -28,28 +28,48 @@ namespace Library.Tests
 
             UserRegister.AddUser(user1);
             UserRegister.AddUser(user2);
+
+            game = new Game(user1, user2);
+
+            GamesRegister.AddGame(game);
         }
 
         [Test]
-        public void TestSearchGameHandler()
+        public void TestPositionShipsHandle()
         {
-            message.Text = handler.Keywords[0];
+            message.Text = "posicionar barco a1 down";
             string response;
             user1.ChangeStatus("position ships");
-            user2.ChangeGameMode("normal");
-            user2.ChangeStatus($"in {user2.GetGameMode()} game");
 
             IHandler result = handler.Handle(message, out response);
 
             Assert.That(result, Is.Not.Null);
-            Assert.That(response, Is.EqualTo($"{user1.GetPlayer().GetBoardsToPrint()}"));
+            Assert.That(response, Is.EqualTo($"El barco se creó correctamente"));
 
-            message.id = user1.GetID();
-
-            result = handler.Handle(message, out response);
+            for (int i = 2; i < 5; i++)
+            {
+                message.Text = $"posicionar barco a{i} down";
+                result = handler.Handle(message, out response);
+            }
 
             Assert.That(result, Is.Not.Null);
-            Assert.That(response, Is.EqualTo($"{user2.GetPlayer().GetBoardsToPrint()}"));
+            Assert.That(response, Is.EqualTo($"Los barcos estan listos"));
+            
+        }
+
+
+        [TestCase("a1 up")]
+        [TestCase("j10 down")]
+        public void InvalidCoordinates(string coor)
+        {
+            message.Text = $"posicionar barco {coor}";
+            string response;
+            user1.ChangeStatus("position ships");
+
+            IHandler result = handler.Handle(message, out response);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(response, Is.EqualTo("Datos incorrectos, el barco no se creó."));
             
         }
 
